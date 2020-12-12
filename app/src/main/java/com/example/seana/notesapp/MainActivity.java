5b308f1238c486f4eac5418686ba4c10cfb9f36e
seana <seanagius2@gmail.com>
2020-12-12 14:23:07 +0100

    Notes

--- app/src/main/java/com/example/seana/notesapp/MainActivity.java
+++ app/src/main/java/com/example/seana/notesapp/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     ListView listView;
+    // so here we will create an array list that will hold all our notes (we set static so that it is accessed within other intents too)
     static ArrayList<String> toDoList = new ArrayList<String>();
     static ArrayAdapter arrayAdapter;
     SharedPreferences sharedPreferences;
@@ -50,24 +51,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // the shared preferences will exist so as to keep the notes permanent
+        // the shared preferences will exist so as to keep the notes permanent (only removed on deletion)
         sharedPreferences = getApplicationContext().getSharedPreferences("com.example.seana.notesapp", Context.MODE_PRIVATE);
-        // the listview will crate a list of items
+
+        // the listview will crate a list of items (which will later form the basis for the list of notes)
         listView = (ListView) findViewById(R.id.listView);
 
 
         serializer();
 
         // now we will create and set the adapter to follow the items of the array list
-
         arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,toDoList);
-
         listView.setAdapter(arrayAdapter);
 
 
 
 
 
+        // now on pressing every individual lsit item we will send the user to a new intent that logs all the text of the list
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -88,10 +89,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+                // this gets the index of what we will be deleting
                 final int itemToDelete = i;
 
+                // we create an allert to assurre that the user actually wants to remove the item
                 new AlertDialog.Builder(MainActivity.this).setIcon(android.R.drawable.ic_dialog_alert)
                         .setTitle("Are you sure?").setMessage("Do you want to delete that note?")
+                        // this is for postive
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
@@ -100,15 +104,14 @@ public class MainActivity extends AppCompatActivity {
                                 // and notify hte adapterv of the change so that it is reflected in the listview
                                 arrayAdapter.notifyDataSetChanged();
 
+                                // permanently save the changes
                                 SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.seana.notesapp", Context.MODE_PRIVATE);
-
-
                                 try {
                                     sharedPreferences.edit().putString("notesAgain15",ObjectSerializer.serialize(MainActivity.toDoList)).apply();
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
-
+                                // on negative we do nothing
                             }
                         }).setNegativeButton("No",null).show();
 
--- app/src/main/java/com/example/seana/notesapp/otherMainActivity.java
+++ app/src/main/java/com/example/seana/notesapp/otherMainActivity.java
@@ -33,24 +33,23 @@ public class otherMainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
+        // so this is an extra that we picked up so as to keep note of the index of the array item that we may revise
+        // if we are adding a new note this has a default value of -1
         noteId = intent.getIntExtra("noteId",-1);
 
+        // and this is to track whether or not there is in fact permission to change (if not permission granted that means we are adding a new note)
         perm = intent.getStringExtra("permissionGiver");
 
+        // so if it is the case that we opened this activity to revise we will get the text from the arraylist and set the text here to its value
         if (noteId != -1) {
-            Log.i("Chest","Seal");
             editText.setText(MainActivity.toDoList.get(noteId));
             numberAllowance = 1;
-        } else {
-            //MainActivity.toDoList.add("");
-            //MainActivity.arrayAdapter.notifyDataSetChanged();
-            //noteId = MainActivity.toDoList.size() - 1;
-
         }
 
 
 
 
+        // so this will now be to save the changes to the edit text
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -59,34 +58,29 @@ public class otherMainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+                // so only in the case were we are adding a new note we will add a new list item and notify the adapter of the changes
                 if (numberAllowance == 0) {
                     MainActivity.toDoList.add("");
                     MainActivity.arrayAdapter.notifyDataSetChanged();
                     numberAllowance = 1;
                 }
 
+                // so here if a new note we set the index to the last array item so the new note is saved to the adapter as such
                 noteId = MainActivity.toDoList.size() - 1;
 
-                Log.i("We have",perm);
 
                 if (perm.equals("granted")) {
-                    Log.i("Jon","Jones");
                     Intent intent = getIntent();
-                    // problem is that you are overriding to last array each time
+                    // and if we are revising an existing note we get the index that said note represents
                     noteId = intent.getIntExtra("noteId",-1);
                 }
 
-
-                Log.i("The number allowance is",Integer.toString(numberAllowance));
-                Log.i("The number index is",Integer.toString(noteId));
-
+                // and then we set the new or revised note to be the types
                 MainActivity.toDoList.set(noteId,String.valueOf(charSequence));
                 MainActivity.arrayAdapter.notifyDataSetChanged();
 
-
+                // and we and save it permanently
                 SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.seana.notesapp", Context.MODE_PRIVATE);
-
-
                 try {
                     sharedPreferences.edit().putString("notesAgain15",ObjectSerializer.serialize(MainActivity.toDoList)).apply();
                 } catch (IOException e) {
